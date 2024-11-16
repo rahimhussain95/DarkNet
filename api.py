804e43d7e161f7cd765a8b107943af246cf7c7c3
rahimhussain <rahim@Nimbus.home>
2024-11-16 17:32:15 -0600

    docker implementation

--- api.py
+++ api.py
@@ -10,13 +10,13 @@ PASSWORD = os.getenv('SPACE_TRACK_PASS')
 
 def login():
     session = requests.Session()
-    url = "https://www.space-track.org/ajaxauth/login"
+    dataUrl = "https://www.space-track.org/ajaxauth/login"
     payload = {
         'identity': USERNAME,
         'password': PASSWORD
     }
 
-    response = session.post(url, data=payload)
+    response = session.post(dataUrl, data=payload)
 
     if response.status_code == 200:
         print("Login Successful")
@@ -26,5 +26,31 @@ def login():
         return
 
 def get_leo(session):
+    if not session:
+        print("ERROR: Session not Active")
+        return 
+    
+    endpoint = "https://www.space-track.org/basicspacedata/query/class/tle_latest"
+
+    # High-risk LEO object parameters
+    params = {
+        "ODRINAL": 1,
+        "PERIOD": ">=88",
+        "PERIOD": "<=127",
+        "APOGEE": ">=160",
+        "APOGEE": "<=2000",
+        "PERIGEE": ">=160",
+        "PERIGEE": "<=2000", 
+        "DECAYED": "false",
+        "FORMAT": "json",
+    }
+
+    response = session.get(endpoint)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        print("ERROR: Failure to retrive Data")
+        return
     
     
